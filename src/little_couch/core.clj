c14e6dc5f9f2d14c7a347fe3728adfca1c7c1cbd
The Rainmaker <obioraakubue@yahoo.com>
2014-06-23 22:16:18 -0400

    delete database, create documents, delete documents

--- src/little_couch/core.clj
+++ src/little_couch/core.clj
@@ -2,6 +2,9 @@
   (:require [cheshire.core :refer :all])
   (:require [clj-http.client :as client]))
 
+ (defn ^:private database_address
+    ([x] (str (:address x) ":" (:port x) "/" (:database x)))
+    ([x doc_id] (str (:address x) ":" (:port x) "/" (:database x) "/" doc_id)))
 
 (defn db-setup
   [& args]
@@ -10,8 +13,17 @@
               {:database database :address address :port port :username username :password password}))
 
 (defn create [x]
-  (try
-   (parse-string (:body (client/put (str (:address x) ":" (:port x) "/" (:database x)) {:content-type :json :throw-entire-message? true} ))
-    true)
-    (catch Exception e (.getMessage e)
-      (throw e))))
+   (parse-string (:body (client/put (database_address x) {:content-type :json :throw-entire-message? true} ))
+    true))
+
+(defn delete [x]
+   (parse-string (:body (client/delete (database_address x) {:throw-entire-message? true} ))
+    true))
+
+ (defn create_doc [x, doc_id, doc]
+   (parse-string (:body (client/put (database_address x doc_id) {:body (generate-string doc), :throw-entire-message? true} ))
+    true))
+
+ (defn get_doc [x, doc_id]
+   (parse-string (:body (client/get (database_address x doc_id) {:throw-entire-message? true} ))
+    true))
