173946ab132bca5d87df5ed7cdc04818e2ca0fab
The Rainmaker <obioraakubue@yahoo.com>
2014-06-23 22:16:44 -0400

    reload REPL on file modifications

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"][cheshire "5.3.1"][clj-http "0.9.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]}}
   :main little-couch.core)
