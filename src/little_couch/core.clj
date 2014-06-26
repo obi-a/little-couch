0e70902dbee0b55a8f80261431408d400361b3b1
The Rainmaker <obioraakubue@yahoo.com>
2014-06-26 00:29:55 -0400

    refactor and adding cookie authentication

--- src/little_couch/core.clj
+++ src/little_couch/core.clj
@@ -2,41 +2,54 @@
   (:require [cheshire.core :refer :all])
   (:require [clj-http.client :as client]))
 
+(use '[clojure.tools.namespace.repl :only (refresh)])
+
 (defn ^:private database_address
    ([x] (str (:address x) ":" (:port x) "/" (:database x)))
    ([x doc_id] (str (:address x) ":" (:port x) "/" (:database x) "/" doc_id)))
 
+(defn http-options
+   ([] {:as :json, :throw-entire-message? true})
+   ([option-map](merge option-map (http-options))))
+
+(defn auth-session
+  ;fix the errors in the request format
+  [x]
+   (cond
+     (not-any? nil? [(:username x) (:password x)])
+           (client/post (str
+                            (database_address x) "/_session/")
+                            (http-options
+                               {:body (str "name=" (:username x) "&password=" (:password x))
+                                           :content_type "application/x-www-form-urlencoded"}))
+      :else ""))
+
  (defn db-setup
   [& args]
-  (let [{:keys [database address port username password], :or {database "" address "http://127.0.0.1" port "5984" username "" password ""} }
+  (let [{:keys [database address port username password], :or {database "" address "http://127.0.0.1" port "5984" username nil password nil} }
                (first args)]
               {:database database :address address :port port :username username :password password}))
 
 (defn create [x]
-   (parse-string (:body (client/put (database_address x) {:content-type :json :throw-entire-message? true} ))
-    true))
+   (:body (client/put (database_address x) (http-options {:content-type :json}))))
+
 
 (defn delete [x]
-  (parse-string (:body (client/delete (database_address x) {:throw-entire-message? true} ))
-    true))
+  (:body (client/delete (database_address x) (http-options))))
 
 (defn create-doc [x, doc_id, doc]
-  (parse-string (:body (client/put (database_address x doc_id) {:body (generate-string doc), :throw-entire-message? true} ))
-    true))
+  (:body (client/put (database_address x doc_id) (http-options {:body (generate-string doc)}) )))
 
 (defn get-doc [x, doc_id]
-  (parse-string (:body (client/get (database_address x doc_id) {:throw-entire-message? true}))
-    true))
+  (:body (client/get (database_address x doc_id) (http-options))))
 
  (defn delete-doc
    ([x, doc_id] (delete-doc x doc_id (:_rev (get-doc x, doc_id))))
    ([x, doc_id, rev]
-   (parse-string (:body (client/delete (str (database_address x doc_id) "?rev=" rev) {:throw-entire-message? true} ))
-    true)))
+      (:body (client/delete (str (database_address x doc_id) "?rev=" rev) (http-options)))))
 
  (defn update-doc [x, doc_id, data]
-   (parse-string (:body (client/put (database_address x doc_id) {:body (generate-string data), :throw-entire-message? true} ))
-    true))
+   (:body (client/put (database_address x doc_id) (http-options {:body (generate-string data)}))))
 
  (defn edit-doc [x, doc_id, data-map]
     (update-doc x doc_id
