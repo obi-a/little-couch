4c7555f81afb882810c6ba10a9fe85ba893962b4
The Rainmaker <obioraakubue@yahoo.com>
2014-07-23 22:34:10 -0400

    adding unit tests for creating databases

--- test/little_couch/core_test.clj
+++ test/little_couch/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [little-couch.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-create
+  (testing "it creates a database."
+    (let [x (db-setup {:database "test-db"})]
+      (is (= {:ok true} (create x)))
+        (do (delete x))))
+  (testing "it throws an exception when database already exists"
+    (let [x (db-setup {:database "mytest-db"})]
+      (do (create x))
+        (is (thrown? Exception (create x)))
+          (do (delete x))))
+  (testing "throws an exception when it cannot connect to database"
+    (let [x (db-setup {:database "mytest-db" :port "9999"})]
+      (is (thrown? java.net.ConnectException (create x))))))
