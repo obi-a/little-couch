7345fc8e5ceb63e1d04680daae73c8a13c97f958
The Rainmaker <obioraakubue@yahoo.com>
2014-08-20 21:18:36 -0400

    updated project settings

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject little-couch "0.1.0"
   :description "Simple Clojure interface to CouchDB"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/obi-a/little-couch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"][cheshire "5.3.1"][clj-http "0.9.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]}}
-  :main little-couch.core)
+  :signing {:gpg-key "obi.akubue@gmail.com"}
+  :main ^:skip-aot little-couch.core)
